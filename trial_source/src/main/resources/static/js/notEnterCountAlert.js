575771d93ab0a9c3123eebbacc65649437ab3808
k-koike <koikekazuma0729@gmail.com>
2025-07-16 17:31:41 +0900

    fix:Task25 Git誤操作部分の更新

---
+++ trial_source/src/main/resources/static/js/notEnterCountAlert.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const hasNotEnterCount = document.getElementById('hasNotEnterCount').value;
+    if (hasNotEnterCount) {
+        alert("過去の勤怠に未入力があります。");
+    }
+});
